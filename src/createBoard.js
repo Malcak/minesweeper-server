8c0092a79d2ca4cfed5a0f303b974a9e3aa271f9
Malcak <malcakx@gmail.com>
2021-08-22 22:49:58 -0500

    add create board method

--- src/createBoard.js
+++ src/createBoard.js
@@ -1,3 +1,55 @@
-const createBoard = (size, callback) => {};
+const addMine = () => {
+  const rng = Math.random();
+  return rng < 0.1 ? -1 : 0;
+};
+
+const placeMines = (board) => {
+  const size = board.length;
+  for (let index = 0; index < board.length; index++) {
+    const row = Array.from({ length: size }, () => addMine());
+    board[index] = row;
+  }
+
+  return board;
+};
+
+const outBounds = (x, y, size) => {
+  return x < 0 || y < 0 || x >= size || y >= size;
+};
+
+const calcNearMines = (board) => {
+  const size = board.length;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (board[i][j] != -1) {
+        let numMines = 0;
+        numMines +=
+          !outBounds(i - 1, j - 1, size) && board[i - 1][j - 1] == -1 ? 1 : 0;
+        numMines += !outBounds(i - 1, j, size) && board[i - 1][j] == -1 ? 1 : 0;
+        numMines +=
+          !outBounds(i - 1, j + 1, size) && board[i - 1][j + 1] == -1 ? 1 : 0;
+        numMines += !outBounds(i, j - 1, size) && board[i][j - 1] == -1 ? 1 : 0;
+        numMines += !outBounds(i, j + 1, size) && board[i][j + 1] == -1 ? 1 : 0;
+        numMines +=
+          !outBounds(i + 1, j - 1, size) && board[i + 1][j - 1] == -1 ? 1 : 0;
+        numMines += !outBounds(i + 1, j, size) && board[i + 1][j] == -1 ? 1 : 0;
+        numMines +=
+          !outBounds(i + 1, j + 1, size) && board[i + 1][j + 1] == -1 ? 1 : 0;
+        board[i][j] = numMines;
+      }
+    }
+  }
+  return board;
+};
+
+const createBoard = (size, callback) => {
+  let board = Array(size);
+  const flags = Array(size).fill(Array(size).fill(0));
+
+  board = placeMines(board);
+  board = calcNearMines(board);
+
+  callback({ board, flags });
+};
 
 module.exports = createBoard;
